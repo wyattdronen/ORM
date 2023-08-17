034e8e22e024fa9d16f9a6c3c9e38ef4c8196cdc
wyattdronen <137117182+wyattdronen@users.noreply.github.com>
2023-08-17 10:57:52 -0500

    models done

--- models/Category.js
+++ models/Category.js
@@ -5,9 +5,19 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
+  { // define id column
+  id: {
+    type: DataTypes.INTEGER, // data type to INTEGER
+    allowNull: false,  // column cannot be null
+    primaryKey: true,  // set to primary key
+    autoIncrement: true  // auto increment
   },
+  // define 'category_name' column
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+},
   {
     sequelize,
     timestamps: false,
